81f8caacea7fd3359d24f55fbd6834e12d3f75d8
DollarUnderscore <anders.wahlqvist@hotmail.com>
2014-07-29 15:11:31 +0200

    Update of module
    Added Get-TDDeviceHistory. New feature from Telldus Live!, see:
    http://developer.telldus.com/blog/2014/06/25/device_state_history_II

--- Telldus.psm1
+++ Telldus.psm1
@@ -325,3 +325,57 @@ function Set-TDDimmer
         Write-Verbose "Dimming device $DeviceID to level $Level. Result: $Results."
     }
 }
+
+function Get-TDDeviceHistory
+{
+    <#
+    .SYNOPSIS
+    Retrieves all events associated with the specified device.
+
+    .DESCRIPTION
+    This command will list all events associated with the specified device
+
+    .EXAMPLE
+    Get-TDDeviceHistory
+
+    .EXAMPLE
+    Get-TDDeviceHistory | Format-Table
+
+    #>
+
+    [cmdletbinding()]
+    param(
+    [Parameter(Mandatory=$True, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
+    [Alias('id')]
+    [string] $DeviceID)
+
+    if ($Telldus -eq $null) {
+        Write-Error "You must first connect using the Connect-TelldusLive cmdlet"
+        return
+    }
+
+    $PostActionURI="http://live.telldus.com/device/history?id=$DeviceID"
+
+    $HistoryEvents = Invoke-RestMethod -Uri $PostActionURI -WebSession $Global:Telldus | select -ExpandProperty History
+
+    foreach ($HistoryEvent in $HistoryEvents)
+    {
+        $PropertiesToOutput = @{
+                             'DeviceID' = $DeviceID
+                             'State' = switch ($HistoryEvent.state)
+                                       {
+                                             1 { "On" }
+                                             2 { "Off" }
+                                            16 { "Dimmed" }
+                                            default { "Unknown" }
+                                       }
+                             'Statevalue' = $HistoryEvent.statevalue
+                             'Origin' = $HistoryEvent.Origin;
+                             'EventDate' = (Get-Date "1970-01-01 00:00:00").AddSeconds($HistoryEvent.ts)
+                             }
+
+        $returnObject = New-Object -TypeName PSObject -Property $PropertiesToOutput
+
+        Write-Output $returnObject | Select-Object DeviceID, EventDate, State, Statevalue, Origin
+    }
+}
