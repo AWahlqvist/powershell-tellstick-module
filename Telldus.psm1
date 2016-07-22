e5255fa1db1212621d581ea3fb0dbbd2f82d7332
Anders Wahlqvist <anders.wahlqvist@hotmail.com>
2016-07-22 14:25:56 +0200

    Update for Get-TDSensorHistoryData
    Get-TDSensorHistoryData now has the ability to specify start/end dates when collecting sensor history samples.

--- Telldus.psm1
+++ Telldus.psm1
@@ -331,16 +331,12 @@ function Get-TDDeviceHistory
     <#
     .SYNOPSIS
     Retrieves all events associated with the specified device.
-
     .DESCRIPTION
     This command will list all events associated with the specified device
-
     .EXAMPLE
     Get-TDDeviceHistory
-
     .EXAMPLE
     Get-TDDeviceHistory | Format-Table
-
     #>
 
     [cmdletbinding()]
@@ -396,19 +392,42 @@ function Get-TDSensorHistoryData
     .DESCRIPTION
     This command will retrieve the sensor history data of the specified sensor.
     
+    .PARAMETER DeviceID
+    The DeviceID of the sensor which data you want to retrieve.
+
+    .PARAMETER After
+    Specify from which date you would like to retrieve sensor history.
+
+    .PARAMETER Before
+    Specify the "end date" of the data samples.
+    Default value is current date.
+
     .EXAMPLE
     Get-TDSensorHistoryData -DeviceID 123456
 
     .EXAMPLE
     Get-TDSensorHistoryData -DeviceID 123456 | Format-Table
 
+    .EXAMPLE
+    Get-TDSensorHistoryData -DeviceID 123456 -After (get-date).AddDays(-1)
+    
+    Get's the history from yesterday until today
+
     #>
 
-    [cmdletbinding()]
+    [cmdletbinding(DefaultParameterSetName='AllData')]
     param(
-    [Parameter(Mandatory=$True, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true)]
-    [Alias('id')]
-    [string] $DeviceID)
+        [Parameter(Mandatory=$True, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true, ParameterSetName='AllData')]
+        [Parameter(Mandatory=$True, ValueFromPipeline=$true, ValueFromPipelineByPropertyName=$true, ParameterSetName='DateRange')]
+        [Alias('id')]
+        [string] $DeviceID,
+
+        [Parameter(Mandatory=$true, ValueFromPipelineByPropertyName=$true, ParameterSetName='DateRange')]
+        [DateTime] $After,
+
+        [Parameter(Mandatory=$false, ValueFromPipelineByPropertyName=$true, ParameterSetName='DateRange')]
+        [DateTime] $Before
+    )
 
     BEGIN {
         if ($Telldus -eq $null) {
@@ -420,7 +439,23 @@ function Get-TDSensorHistoryData
     PROCESS {
         $PostActionURI="http://live.telldus.com/sensor/history?id=$DeviceID"
 
-        $HistoryDataPoints = Invoke-RestMethod -Uri $PostActionURI -WebSession $Global:Telldus | select -ExpandProperty History
+        if ($PSCmdlet.ParameterSetName -eq 'DateRange') {
+            if (-not $Before) {
+                $Before = (Get-Date).ToUniversalTime()
+            }
+
+            if ($Before -gt $After) {
+                $FromDateToPost = [Math]::Floor((New-TimeSpan -Start '1970-01-01' -End $After.ToUniversalTime()).TotalSeconds)
+                $ToDateToPost = [Math]::Floor((New-TimeSpan -Start '1970-01-01' -End $Before.ToUniversalTime()).TotalSeconds)
+
+                $PostActionURI = $PostActionURI + "&fromdate=$FromDateToPost" + "&todate=$ToDateToPost"
+            }
+            else {
+                throw 'The value for Before must be greater than the value for After.'
+            }
+        }
+
+        $HistoryDataPoints = Invoke-RestMethod -Uri $PostActionURI -WebSession $Global:Telldus | Select-Object -ExpandProperty History
 
         foreach ($HistoryDataPoint in $HistoryDataPoints)
         {
