7fd766997da268f178e7bcab4f1558439362670d
AWahlqvist <anders.wahlqvist@hotmail.com>
2018-02-08 10:30:00 +0100

    Updated buildscript to generate a better module manifest

--- build.ps1
+++ build.ps1
@@ -1,42 +1,84 @@
 $ScriptRoot = $PSScriptRoot
 
+$ReleaseNotes = 'Inner beatuy update (module manifest)'
+$ModuleName = 'AutomaTD'
+$ModuleVersion = '1.0.5'
+$CompatiblePSEditions = @('Core', 'Desktop')
+$PowerShellVersion = '5.1'
+$ModuleGuid = '7acb0966-268a-4bd9-8ce8-c546db8aa696'
+$Author = 'Anders Wahlqvist'
+$CompanyName = 'DollarUnderscore'
+$Description = 'A module that can interact Telldus Live!'
+$ProjectUri = 'https://github.com/AWahlqvist/powershell-tellstick-module'
+$LicenseUri = 'https://github.com/AWahlqvist/powershell-tellstick-module/blob/master/LICENSE.txt'
+
 $ModuleToBuild = @{
-    Name = 'AutomaTD'
+    Name = $ModuleName
     Directory = Join-Path -Path $ScriptRoot -ChildPath .\source
-    Fullname = Join-Path -Path $ScriptRoot -ChildPath .\source\AutomaTD.psd1
+    Fullname = Join-Path -Path $ScriptRoot -ChildPath ".\source\$ModuleName.psd1"
 }
+
+$ManifestExportPath = ''
 
 # Make sure we have the latest version of the module loaded in memory
 Get-Module $ModuleToBuild.Name | Remove-Module -Force
 Import-Module $ModuleToBuild.FullName -Verbose:$false
-$ModuleInfo = Get-Module $ModuleToBuild.Name
 
 
-Write-Output "Preparing $($ModuleInfo.Name)..."
+Write-Output "Preparing $ModuleName..."
 
 $PreparedModulePath = Join-Path -Path $ScriptRoot -ChildPath "Release\$($ModuleToBuild.Name)"
+$PublishModulePath = Join-Path -Path $ScriptRoot -ChildPath "Publish\"
 
 if (Test-Path $PreparedModulePath) {
     Remove-Item $PreparedModulePath -Recurse -Force
 }
 
 $null = New-Item -Path $PreparedModulePath -ItemType Directory -Force
+$null = New-Item -Path $PublishModulePath -ItemType Directory -Force
 
 Get-ChildItem -Path $ModuleToBuild.Directory | Copy-Item -Destination $PreparedModulePath -Force -Recurse
     
 $ModuleFile = Get-ChildItem -Path $PreparedModulePath -Include *.psm1 -Recurse
+$ManifestFile = Get-ChildItem -Path $PreparedModulePath -Include *.psd1 -Recurse
 
 $FunctionFiles = @( Get-ChildItem -Path "$($ModuleToBuild.Directory)\Private\*.ps1" -ErrorAction Stop)
-$FunctionFiles += Get-ChildItem -Path "$($ModuleToBuild.Directory)\Public\*.ps1" -ErrorAction Stop
+$PublicFunctions = Get-ChildItem -Path "$($ModuleToBuild.Directory)\Public\*.ps1" -ErrorAction Stop
+$FunctionFiles += $PublicFunctions
 
 $ModuleCode = $FunctionFiles | Get-Content
 
 [System.IO.File]::WriteAllLines($ModuleFile.FullName, $ModuleCode)
 
+$ManifestCreationParams = @{
+    RootModule = "$ModuleName.psm1"
+    ModuleVersion = $ModuleVersion
+    CompatiblePSEditions = $CompatiblePSEditions
+    Guid = $ModuleGuid
+    Author = $Author
+    Description = $Description
+    CompanyName = $CompanyName
+    Copyright = "(c) $((Get-Date).Year) $Author. All rights reserved."
+    PowerShellVersion = $PowerShellVersion
+    FunctionsToExport = $PublicFunctions.BaseName
+    ReleaseNotes = $ReleaseNotes
+    ProjectUri = $ProjectUri
+    LicenseUri = $LicenseUri
+    Path = $ManifestFile.FullName
+}
+
+New-ModuleManifest @ManifestCreationParams
+
 Get-ChildItem -Path $PreparedModulePath -Recurse | Where-Object { $_.FullName -match '\\Tests|\\Public|\\Private|\\TestResources'} | Remove-Item -Force -Recurse
 
-$ZipArhive = Join-Path -Path $ScriptRoot -ChildPath "Release\$($ModuleToBuild.Name).$($ModuleInfo.Version).zip"
+Get-ChildItem -Path $PublishModulePath -Recurse | Remove-Item -Force -Recurse
+
+Copy-Item -Path $PreparedModulePath -Destination $PublishModulePath -Recurse
+
+$ZipArhive = Join-Path -Path $ScriptRoot -ChildPath "Release\$($ModuleToBuild.Name).$ModuleVersion.zip"
 
 Compress-Archive -Path $PreparedModulePath -DestinationPath $ZipArhive -Force
 
 Remove-Item -Path $PreparedModulePath -Force -Recurse
+
+$ModuleToPublish = Join-Path -Path $PublishModulePath -ChildPath $ModuleName
